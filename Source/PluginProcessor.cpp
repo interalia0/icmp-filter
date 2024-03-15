f703c825962a5d9a43298a89464ba41dbdc46edc
interalia_0 <elja@below0.online>
2024-03-15 10:33:19 +0200

    reset state when changing filter types

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -143,12 +143,6 @@ void ICMPfilterAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
@@ -158,6 +152,7 @@ void ICMPfilterAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     // the samples and the outer loop is handling the channels.
     // Alternatively, you can process the samples with the channels
     // interleaved by keeping the same state.
+    
     for (int channel = 0; channel < totalNumInputChannels; ++channel) {
             auto* in = buffer.getReadPointer (channel);
             auto* out = buffer.getWritePointer (channel);
@@ -225,6 +220,7 @@ void ICMPfilterAudioProcessor::parameterChanged(const juce::String &parameterID,
     }
     
     if (parameterID == "fType") {
+        filter.reset();
         filter.setType(newValue);
     }
 }
